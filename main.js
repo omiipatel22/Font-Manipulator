da548518e0900376184592d9c4aa2fc7d131421f
Om <107304353+omiipatel22@users.noreply.github.com>
2022-12-01 17:02:45 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550,500)
+
+    canvas = createCanvas(550, 550);
+    canvas.position(560, 150);
+
+    poseNet = ml5.posenet(video, modelLoaded);
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initizalied!');
+}
+
+function draw() {
+    background('#969A97');
+}
+
